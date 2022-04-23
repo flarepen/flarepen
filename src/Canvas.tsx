3f4982fe28b945a0249e5420a7bc630e01c5e915
Balagopal Kanattil <balukanattil@gmail.com>
2022-04-24 01:56:20 +0530

    Fix blury canvas and add basic use of Context

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { debounce  } from "lodash";
 
 function Canvas(): JSX.Element {
@@ -7,6 +7,11 @@ function Canvas(): JSX.Element {
         height: window.innerHeight,
     });
 
+    const scale = window.devicePixelRatio;
+
+    const styles = { display: "block", width: "100vw", height: "100vh" }
+
+    // Handle Resize
     useEffect(() => {
         function handleWindowResize() {
             setDimensions({
@@ -22,8 +27,27 @@ function Canvas(): JSX.Element {
         return () => window.removeEventListener('resize', debouncedHandler);
     });
 
+    const canvasRef = useRef(null);
+
+    // Get Context. TODO: Possibly a new hook?
+    useEffect(() => {
+        if (canvasRef.current) {
+            const canvas: HTMLCanvasElement = canvasRef.current;
+            const ctx = canvas.getContext("2d")!; // TODO: better error handling?
+            ctx.font = "24px Monaco";
+            ctx.scale(scale, scale);
+            // TODO: Get draw from props and with state observers?
+            ctx.fillText("FlarePen!", 100, 100);
+        }
+    });
+
     return (
-        <canvas width={dimensions.width} height={dimensions.height} aria-label="ascii canvas">
+        <canvas id="canvas"
+            ref={canvasRef}
+            width={dimensions.width * scale}
+            height={dimensions.height * scale}
+            style={styles}
+            aria-label="ascii canvas">
             <div>Test</div>
         </canvas>
     );
