c67787c5b58a2c896f60b55edc6fe3e89c601a45
Balagopal Kanattil <balukanattil@gmail.com>
2023-08-19 23:22:03 -0400

    Use more intuitive shortcuts for tools

--- apps/web/src/tools.ts
+++ apps/web/src/tools.ts
@@ -8,14 +8,10 @@ export enum Tool {
   Select = 'Select',
 }
 
-export const TOOL_SHORTCUTS: { [key in Tool]: string } = {
-  [Tool.Rectangle]: '1',
-  [Tool.Line]: '2',
-  [Tool.Arrow]: '3',
-  [Tool.Text]: '4',
-  [Tool.Select]: '5',
-};
-
-export const SHORTCUT_TO_TOOL = _.invert(TOOL_SHORTCUTS) as {
-  [i: string]: Tool;
-};
+export const SHORTCUT_TO_TOOL = {
+  r: Tool.Rectangle,
+  l: Tool.Line,
+  a: Tool.Arrow,
+  t: Tool.Text,
+  s: Tool.Select,
+} as { [i: string]: Tool };
