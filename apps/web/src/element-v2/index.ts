542ee1c01b3aa098b0528758766a6ffb92c0ca88
Balagopal Kanattil <balukanattil@gmail.com>
2025-11-25 22:39:46 -0500

    Cleanup v2

--- apps/web/src/element-v2/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export * from './registry';
-export { LineHandler } from './handlers/line';
-export { ArrowHandler } from './handlers/arrow';
-export { RectangleHandler } from './handlers/rectangle';
-export { TextHandler } from './handlers/text';
